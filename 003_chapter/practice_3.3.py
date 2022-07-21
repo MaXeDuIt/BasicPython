bc463ab897bd00331d3f52b28bcca731035a5020
MaxAdmin <maxeduit2@gmail.com>
2022-07-21 16:36:01 +0300

    Practice_3.3, part 1 is done

--- 003_chapter/practice_3.3.py
+++ 003_chapter/practice_3.3.py
@@ -1,1 +1,39 @@
 
+def do_twice(func):
+    func()
+    func()
+
+def do_four(func):
+    do_twice(func)
+    do_twice(func)
+
+def plus_minus_space():
+    print('+ - - - -', end=' ')
+
+def plus():
+    print('+')
+
+def slash_space():
+    print('|        ', end=' ')
+
+def slash():
+    print('|')
+
+def first_line():
+    do_twice(plus_minus_space)
+    plus()
+
+def second_line():
+    do_twice(slash_space)
+    slash()
+
+def grid():
+    first_line()
+    do_four(second_line)
+
+def draw_grid():
+    do_twice(grid)
+    first_line()
+
+
+draw_grid()
