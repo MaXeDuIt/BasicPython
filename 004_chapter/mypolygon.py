ce04746e9ea7bb52ee040593490c13fb578bb73e
MaxAdmin <maxeduit2@gmail.com>
2022-07-22 12:07:17 +0300

    Chapter 4, practices in the theoretical part - practice 5 is done

--- 004_chapter/mypolygon.py
+++ 004_chapter/mypolygon.py
@@ -8,20 +8,27 @@ def square(t, length):
 
 def polygon(t, n, length):
     angle = 360 / n
+    polyline(t, n, length, angle)
+
+def circle(t, r):
+    arc(t, r, 360)
+
+def arc(t, r, angle):
+    arc_length = (math.pi * r / 180) * angle
+    n = int(arc_length / 3) + 1
+    step_length = arc_length / n
+    step_angle = angle / n
+    polyline(t, n, step_length, step_angle)
+
+def polyline(t, n, length, angle):
     for i in range(n):
         t.fd(length)
         t.lt(angle)
 
-def circle(t, r):
-    circumference = 2 * math.pi * r
-    n = int(circumference / 3) + 1
-    length = circumference / n
-    polygon(t, n, length)
-
-
 bob = turtle.Turtle()
 # square(bob, 175)
 # polygon(bob, 7, 120)
-circle(bob, 150)
+# circle(bob, 150)
+arc(bob, 100, 360)
 
 turtle.mainloop()
