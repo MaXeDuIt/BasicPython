f856ce9853757a623e428385e05c732438f25cd1
MaxAdmin <maxeduit2@gmail.com>
2022-07-22 10:38:09 +0300

    Chapter 4, practices in the theoretical part - practice 4 is done - practice 3: added param "angle"

--- 004_chapter/mypolygon.py
+++ 004_chapter/mypolygon.py
@@ -1,5 +1,5 @@
 
-import turtle
+import turtle, math
 
 def square(t, length):
     for i in range(4):
@@ -7,12 +7,20 @@ def square(t, length):
         t.lt(90)
 
 def polygon(t, n, length):
+    angle = 360 / n
     for i in range(n):
         t.fd(length)
-        t.lt(360 / n)
+        t.lt(angle)
+
+def circle(t, r):
+    n = 50
+    circumference = 2 * math.pi * r
+    length = circumference / n
+    polygon(t, n, length)
 
 bob = turtle.Turtle()
 # square(bob, 175)
-polygon(bob, 7, 120)
+# polygon(bob, 7, 120)
+circle(bob, 50)
 
 turtle.mainloop()
