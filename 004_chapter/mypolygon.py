39914020943bb3b74e70a397940c6a018f37da00
MaxAdmin <maxeduit2@gmail.com>
2022-07-22 11:02:18 +0300

    Chapter 4, practices in the theoretical part - practice 4 is changed param "n"

--- 004_chapter/mypolygon.py
+++ 004_chapter/mypolygon.py
@@ -13,14 +13,15 @@ def polygon(t, n, length):
         t.lt(angle)
 
 def circle(t, r):
-    n = 50
     circumference = 2 * math.pi * r
+    n = int(circumference / 3) + 1
     length = circumference / n
     polygon(t, n, length)
+
 
 bob = turtle.Turtle()
 # square(bob, 175)
 # polygon(bob, 7, 120)
-circle(bob, 50)
+circle(bob, 150)
 
 turtle.mainloop()
