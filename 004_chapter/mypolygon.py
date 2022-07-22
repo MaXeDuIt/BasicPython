0e2ca08b528065d8ec7fdbabbf749928e58d20b8
MaxAdmin <maxeduit2@gmail.com>
2022-07-22 10:25:09 +0300

    Chapter 4, practices in the theoretical part - practice 3 is done

--- 004_chapter/mypolygon.py
+++ 004_chapter/mypolygon.py
@@ -6,7 +6,13 @@ def square(t, length):
         t.fd(length)
         t.lt(90)
 
+def polygon(t, n, length):
+    for i in range(n):
+        t.fd(length)
+        t.lt(360 / n)
+
 bob = turtle.Turtle()
-square(bob, 175)
+# square(bob, 175)
+polygon(bob, 7, 120)
 
 turtle.mainloop()
